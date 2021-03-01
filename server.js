f010888609207ea03535cd90b32f0cdf94562e37
Christian Martinez <christian@mixdlabs.com>
2021-03-01 08:06:52 -0700

    Heart button needs to sync with client, working on a fix, only works on page refresh

--- server.js
+++ server.js
@@ -29,30 +29,45 @@ const MysteryBoard = mongoose.model('MysteryBoard', mysteryBoardSchema)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/mysteryboards', (req, res) => {
-    MysteryBoard.find({}, (err, posts) => { 
+    MysteryBoard.find({}, (err, mysteryboards) => { 
         res.render('mysteryboards', {
             dayjs: dayjs,
-            mysteryBoards: posts
+            mysteryBoards: mysteryboards
         })
     }).sort({date: -1}) // Sort from newest to oldest
 
 })
 
-app.post('/', (req, res) => {
+app.post('/', (req, res) =>  {
     let newMysteryBoard = new MysteryBoard(
     { name: req.body.name, content: req.body.content, date: req.body.date, hearts: 0 }
     )
-    console.log(newMysteryBoard.hearts)
     newMysteryBoard.save()
     res.redirect('/mysteryboards')
 })
 
-app.post('/mysteryboards/:id', (req, res) => {
-    //MysteryBoard.findByIdAndUpdate(
-    //    req.params.id,
-    //)
-    console.log(req.params.id)
-    console.log(req.body)
+// We have two endpoints - heart && unheart. 
+// User hearts the post, goes to heart endpoint and the db should increment
+// and update the heart by 1. /unheart should decrement the heart count by -1. 
+// This works perfectly. The only problem is that the client has to refresh the page
+// to view the updated data. Ideally, it should be synced. 
+
+app.put('/mysteryboards/:id/heart', async (req, res) => {
+    try {
+        const increment = await MysteryBoard.findByIdAndUpdate(req.params.id, { $inc: { hearts: 1 }}, { new: true })
+        console.log(increment.hearts) // 1 { hearts: 1 }
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+app.put('/mysteryboards/:id/unheart', async (req, res) => {
+    try {
+        const decrement = await MysteryBoard.findByIdAndUpdate(req.params.id, { $inc: { hearts: -1 }}, { new: true })
+        console.log(decrement.hearts) // { hearts: 0 }
+    } catch (err) {
+        console.log(err.message)
+    }
 })
 
 app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`)) // Listen for a client connection
