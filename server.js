4f79a4aebf3c2765d82bbc11ef19b80160c0391c
Christian Martinez <christian@mixdlabs.com>
2021-03-03 16:37:52 -0700

    adf

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ require('dotenv').config()
 const bodyParser = require('body-parser') // Parse bodies
 const ejs = require('ejs') // We are using ejs to render the data from database out to the page
 
-
-app.set('view engine', 'ejs') 
-
 app.use(bodyParser.urlencoded({ extended: true })) // Middleware for parsing request bodies
 
 mongoose.connect(process.env.MONGO_DB, { 
@@ -28,7 +25,8 @@ const mysteryBoardSchema = mongoose.Schema( // Mysteryboard Schema
 const MysteryBoard = mongoose.model('MysteryBoard', mysteryBoardSchema) // Model
 
 app.use(express.static(path.join(__dirname, 'public'))) // /public
-
+app.set('view engine', 'ejs') 
+app.set('views', path.join(__dirname, 'views'))
 app.get('/mysteryboards', (req, res) => { // /mysteryboards 
     MysteryBoard.find({}, (err, mysteryboards) => { // Search mongo for mysteryboards data
         res.render('mysteryboards', { // Render the data to the page
